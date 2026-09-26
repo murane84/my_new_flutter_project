33ff6c36f2369efbbab8fe6c793d80e62aed4753
murane84 <murane84@gmail.com>
2026-09-26 23:55:25 +0300

    Deploy latest web build

--- my_aluta_api/webapp/flutter_bootstrap.js
+++ my_aluta_api/webapp/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4128828064" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3077167343" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
